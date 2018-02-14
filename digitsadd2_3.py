39607a8657f93025539016ec2ee95107cc4677e2
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2018-02-14 19:45:36 +0200

    Now works with A(n+1)>=A(n) and not A(n+1)>A(n)
    Previous version was working with A(n+1)>A(n).
    This version works woth A(n+1)>=A(n).
    prev: (limit_n=9,limit_s=45=1+2+3+4+5+6+7+8+9). 
    now:(limit_n=none,limit_s=none).
    example
    (n=3,s=12)
    prev:129,138,147,156,237,246,345,39,48,57 (counter=10)
    now:129,138,147,156,228,237,246,255,336,345,39,444(x),48,57,66(x) (counter=15-2(x)=13).
    notice tht 444 and 66 sum up to 12 but the digits are not rising(idk what they do but they're def. not rising )

--- digitsadd2_3.py
+++ digitsadd2_3.py
@@ -1,22 +1,27 @@
-print("*Gives amount of numbers (of N digits) that their ascending digits adds to a Sum(S)*")
-n = int(input("Give number of digits 'N' (from 1 to 9 'for obvious reasons'): "))
-while (n<1 or n>9):
-	n = int(input("Give number of digits 'N' (from 1 to 9 'for obvious reasons'): "))
-s = int(input("Give number of digits 'S' (from 1 to 45 'for obvious reasons'): "))
-while (s<1 or s>45):
-	s = int(input("Give number of digits 'S' (from 1 to 45 'for obvious reasons'): "))
+print("*Gives amount of numbers (of N digits) that their rising digits adds to a Sum(S)*")
+n = int(input("Give number of digits 'N': "))
+while (n<1):
+	n = int(input("Give number of digits 'N': "))
+s = int(input("Give number of digits 'S': "))
+while (s<1):
+	s = int(input("Give number of digits 'S': "))
 c=0
-def loop(s,start,dig,maxdig,c):
+def loop(s,start,dig,c):
 	flag = 0
 	i=start
 	while i < 10 and flag==0:
 		if i==s:
 			flag=1
-		elif dig < maxdig:
-			c = loop(s-i,i+1,dig+1,maxdig,c)
+		elif dig < n:
+			c = loop(s-i,i,dig+1,c)
 		i=i+1
 	if flag==1:
+		#print((dig-1)*"#"+str(i-1))
 		c=c+1
 	return c
-c = loop(s,1,1,n,c)
-print("Amount of numbers that their digits adds to S: "+str(c))
+c = loop(s,1,1,c)
+for i in range(1,10):
+	for j in range(2,n+1):
+		if j*i==s:
+			c-=1
+print("Amount of numbers that their rising digits adds to S: "+str(c))
